b169c7eb81a35f35e8e0c755a68c9e28c396bfd4
spiralizing <spiralizing@gmail.com>
2018-10-13 10:24:23 -0500

    some few changes in LSTM

--- Recurrent_NN/LSTM.py
+++ Recurrent_NN/LSTM.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
@@ -14,7 +14,7 @@ print(X_train[0])
 max_review_length = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
 X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
-
+np.shape(y_train)
 print(X_train[0])
 print(y_train[:10])
 
@@ -28,7 +28,7 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 print(model.summary())
 
 #
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=64)
 
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
